85bff75b4f6e1a13658f14c2e82a34619fa1c70f
escapingtime <gharris@mobileinstallusa.com>
2022-08-01 13:16:35 -0700

    Added continuous movement functionality to Ship game object.

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,6 +24,7 @@ class AlienInvasion:
 		"""Start the main loop for the game."""
 		while True:
 			self._check_events()
+			self.ship.update()
 			self._update_screen()
 
 			# Make the most recently drawn screen visible.
@@ -36,8 +37,10 @@ class AlienInvasion:
 					sys.exit()
 				elif event.type == pygame.KEYDOWN:
 					if event.key == pygame.K_RIGHT:
-						# Move the ship to the right.
-						self.ship.rect.x += 1
+						self.ship.moving_right = True
+				elif event.type == pygame.KEYUP:
+					if event.key == pygame.K_RIGHT:
+						self.ship.moving_right = False
 
 	def _update_screen(self):
 		"""Update images on the screen, and flip to a new screen."""
