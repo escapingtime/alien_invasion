64c2d9bdf8723d957887abfdcb26a8123387ae31
escapingtime <gharris@mobileinstallusa.com>
2022-08-04 06:11:53 -0700

    Refactored _check_events() into multiple helper modules.

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,19 +33,26 @@ class AlienInvasion:
 	def _check_events(self):
 		"""Respond to keypress and mouse events."""
 		for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					sys.exit()
-				elif event.type == pygame.KEYDOWN:
-					if event.key == pygame.K_RIGHT:
-						self.ship.moving_right = True
-					elif event.key == pygame.K_LEFT:
-						self.ship.moving_left = True
+			if event.type == pygame.QUIT:
+				sys.exit()
+			if event.type == pygame.KEYDOWN:
+				self._check_keydown_events(event)
+			elif event.type == pygame.KEYUP:
+				self._check_keyup_events(event)
 
-				elif event.type == pygame.KEYUP:
-					if event.key == pygame.K_RIGHT:
-						self.ship.moving_right = False
-					elif event.key == pygame.K_LEFT:
-						self.ship.moving_left = False
+	def _check_keydown_events(self, event):
+		"""Respond to keypresses."""
+		if event.key == pygame.K_RIGHT:
+			self.ship.moving_right = True
+		elif event.key == pygame.K_LEFT:
+			self.ship.moving_left = True
+
+	def _check_keyup_events(self, event):
+		"""Respond to key releases."""
+		if event.key == pygame.K_RIGHT:
+			self.ship.moving_right = False
+		elif event.key == pygame.K_LEFT:
+			self.ship.moving_left = False
 
 	def _update_screen(self):
 		"""Update images on the screen, and flip to a new screen."""
