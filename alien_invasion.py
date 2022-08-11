8215a96f40b7ccf79c91f9fd5b4dd84258a90453
escapingtime <gharris@mobileinstallusa.com>
2022-08-11 10:50:53 -0700

    Added functionality to delete bullets once off screen, and esc to quit.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,9 +1,9 @@
 import sys
-
 import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
 	"""Overall class to manage game assets and behavior."""
@@ -28,6 +28,13 @@ class AlienInvasion:
 			self._check_events()
 			self.ship.update()
 			self.bullets.update()
+
+			# Get rid of bullets that have disappeared.
+			for bullet in self.bullets.copy():
+				if bullet.rect.bottom <= 0:
+					self.bullets.remove(bullet)
+
+
 			self._update_screen()
 
 			# Make the most recently drawn screen visible.
@@ -49,8 +56,12 @@ class AlienInvasion:
 			self.ship.moving_right = True
 		elif event.key == pygame.K_LEFT:
 			self.ship.moving_left = True
+		elif event.key == pygame.K_ESCAPE:
+			sys.exit()
 		elif event.key == pygame.K_q:
 			sys.exit()
+		elif event.key == pygame.K_SPACE:
+			self._fire_bullet()
 
 	def _check_keyup_events(self, event):
 		"""Respond to key releases."""
@@ -59,10 +70,18 @@ class AlienInvasion:
 		elif event.key == pygame.K_LEFT:
 			self.ship.moving_left = False
 
+	def _fire_bullet(self):
+		"""Create a new bullet and add it to the bullets group."""
+		new_bullet = Bullet(self)
+		self.bullets.add(new_bullet)
+
 	def _update_screen(self):
 		"""Update images on the screen, and flip to a new screen."""
 		self.screen.fill(self.settings.bg_color)
 		self.ship.blitme()
+		for bullet in self.bullets.sprites():
+			bullet.draw_bullet()
+			pygame.display.flip()
 
 if __name__ == '__main__':
 	# Make a game instance, and run the game.
--- settings.py
+++ settings.py
@@ -12,11 +12,11 @@ class Settings:
 		self.ship_speed = 1.5
 
 		# Bullet settings
-		self.bullet_speed = 1.0
+		self.bullet_speed = 5.0
 		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_color = (60, 60, 60)
 
-	
+
 
 		
