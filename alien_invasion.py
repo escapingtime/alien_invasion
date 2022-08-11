7ca7bcbd0dbd78144fe762d36775ade844816faf
escapingtime <gharris@mobileinstallusa.com>
2022-08-11 14:00:20 -0700

    Added functionality to limit total number of bullets in game.

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,14 +27,7 @@ class AlienInvasion:
 		while True:
 			self._check_events()
 			self.ship.update()
-			self.bullets.update()
-
-			# Get rid of bullets that have disappeared.
-			for bullet in self.bullets.copy():
-				if bullet.rect.bottom <= 0:
-					self.bullets.remove(bullet)
-
-
+			self._update_bullets()
 			self._update_screen()
 
 			# Make the most recently drawn screen visible.
@@ -72,8 +65,19 @@ class AlienInvasion:
 
 	def _fire_bullet(self):
 		"""Create a new bullet and add it to the bullets group."""
-		new_bullet = Bullet(self)
-		self.bullets.add(new_bullet)
+		if len(self.bullets) < self.settings.bullets_allowed:
+			new_bullet = Bullet(self)
+			self.bullets.add(new_bullet)
+
+	def _update_bullets(self):
+		"""Update position of bullets and get rid of old bullets."""
+		# Update bullet positions
+		self.bullets.update()
+
+		# Get rid of bullets that have disappeared.
+		for bullet in self.bullets.copy():
+			if bullet.rect.bottom <= 0:
+				self.bullets.remove(bullet)
 
 	def _update_screen(self):
 		"""Update images on the screen, and flip to a new screen."""
