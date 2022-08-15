37e36371bcc2c8848b8d31849589917b07a87dbc
escapingtime <gharris@mobileinstallusa.com>
2022-08-15 14:37:50 -0700

    Refactored _create_fleet() to fill screen horizontally with aliens.

--- alien_invasion.py
+++ alien_invasion.py
@@ -85,10 +85,20 @@ class AlienInvasion:
 
 	def _create_fleet(self):
 		"""Create the fleet of aliens."""
-		# Make an alien.
+		# Create an alien and find the number of aliens in a row.
+		# Spacing between each alien is equal to one alien width.
 		alien = Alien(self)
-		self.aliens.add(alien)
+		alien_width = alien.rect.width
+		avaliable_space_x = self.settings.screen_width - (2 * alien_width)
+		number_aliens_x = avaliable_space_x // (2 * alien_width)
 
+		# Create the first row of aliens.
+		for alien_number in range(number_aliens_x):
+			alien = Alien(self)
+			alien.x = alien_width + 2 * alien_width * alien_number
+			alien.rect.x = alien.x
+			self.aliens.add(alien)
+			
 	def _update_screen(self):
 		"""Update images on the screen, and flip to a new screen."""
 		self.screen.fill(self.settings.bg_color)
