0d7ff42c328646987a16e0a201b14df82a1db7e6
escapingtime <gharris@mobileinstallusa.com>
2022-08-23 14:10:16 -0700

    Added collision detection for bullets and aliens. Added code to repopulate fleet upon destruction.

--- alien_invasion.py
+++ alien_invasion.py
@@ -84,6 +84,16 @@ class AlienInvasion:
 			if bullet.rect.bottom <= 0:
 				self.bullets.remove(bullet)
 
+		# Check for any bullets that have hit aliens.
+		#  If so, get rid of the bullet and the alien.
+		collisions = pygame.sprite.groupcollide(
+			self.bullets, self.aliens, True, True)
+
+		if not self.aliens:
+			# Destroy existing bullets and create new fleet.
+			self.bullets.empty()
+			self._create_fleet()
+
 	def _update_aliens(self):
 		"""
 		Check if the fleet is at an edge,
