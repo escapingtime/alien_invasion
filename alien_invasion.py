c7bc281406c723e5b81bf9103901d24225c4dcf8
escapingtime <gharris@mobileinstallusa.com>
2022-08-24 16:42:35 -0700

    Added game_active flag to end the game.

--- alien_invasion.py
+++ alien_invasion.py
@@ -163,19 +163,22 @@ class AlienInvasion:
 
 	def _ship_hit(self):
 		"""Respond to the ship being hit by an alien."""
-		# Decrement ships_left.
-		self.stats.ships_left -= 1
+		if self.stats.ships_left > 0:
+			# Decrement ships_left.
+			self.stats.ships_left -= 1
 
-		# Get rid of any remaining aliens and bullets.
-		self.aliens.empty()
-		self.bullets.empty()
+			# Get rid of any remaining aliens and bullets.
+			self.aliens.empty()
+			self.bullets.empty()
 
-		# Create a new fleet and center the ship.
-		self._create_fleet()
-		self.ship.center_ship()
+			# Create a new fleet and center the ship.
+			self._create_fleet()
+			self.ship.center_ship()
 
-		# Pause.
-		sleep(0.5)
+			# Pause.
+			sleep(0.5)
+		else:
+			self.stats.game_active = False
 
 	def _check_aliens_bottom(self):
 		"""Check if any aliens have reached the bottom of the screen."""
