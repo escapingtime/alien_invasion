651cbbe9c3149eef3e5f3b6e8d443da6caea1c62
escapingtime <gharris@mobileinstallusa.com>
2022-08-24 16:32:26 -0700

    Added _check_aliens_bottom() for functionality.

--- alien_invasion.py
+++ alien_invasion.py
@@ -116,6 +116,9 @@ class AlienInvasion:
 		if pygame.sprite.spritecollideany(self.ship, self.aliens):
 			self._ship_hit()
 
+		# Look for aliens hitting the bottom of the screen.
+		self._check_aliens_bottom()
+
 	def _create_fleet(self):
 		"""Create the fleet of aliens."""
 		# Create an alien and find the number of aliens in a row.
@@ -173,6 +176,15 @@ class AlienInvasion:
 
 		# Pause.
 		sleep(0.5)
+
+	def _check_aliens_bottom(self):
+		"""Check if any aliens have reached the bottom of the screen."""
+		screen_rect = self.screen.get_rect()
+		for alien in self.aliens.sprites():
+			if alien.rect.bottom >= screen_rect.bottom:
+				# Treat this the same as if the ship got hit.
+				self._ship_hit()
+				break
 
 	def _update_screen(self):
 		"""Update images on the screen, and flip to a new screen."""
