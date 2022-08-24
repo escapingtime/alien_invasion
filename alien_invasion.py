6c2f2891a01a3be0af4c20e4e09b59c8da068f97
escapingtime <gharris@mobileinstallusa.com>
2022-08-24 14:11:06 -0700

    Added GameStats class and corresponding functionality to alien_invasion.py; Refactored settings.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,10 @@
 import sys
+from time import sleep
+
 import pygame
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -17,6 +20,10 @@ class AlienInvasion:
 		self.settings.screen_width = self.screen.get_rect().width
 		self.settings.screen_height = self.screen.get_rect().height
 		pygame.display.set_caption("Alien Invasion")
+
+		# Create an instance to store game statistics.
+		self.stats = GameStats(self)
+
 		self.ship = Ship(self)
 		self.bullets = pygame.sprite.Group()
 		self.aliens = pygame.sprite.Group()
@@ -97,7 +104,6 @@ class AlienInvasion:
 			self.bullets.empty()
 			self._create_fleet()
 
-
 	def _update_aliens(self):
 		"""
 		Check if the fleet is at an edge,
@@ -105,6 +111,10 @@ class AlienInvasion:
 		"""
 		self._check_fleet_edges()
 		self.aliens.update()
+
+		# Look for alien-ship collisions.
+		if pygame.sprite.spritecollideany(self.ship, self.aliens):
+			print("Ship hit!!!")
 
 	def _create_fleet(self):
 		"""Create the fleet of aliens."""
--- settings.py
+++ settings.py
@@ -10,7 +10,8 @@ class Settings:
 
 		# Ship settings
 		self.ship_speed = 1.5
-
+		self.ship_limit = 3
+		
 		# Bullet settings
 		self.bullet_speed = 6
 		self.bullet_width = 3
@@ -20,7 +21,7 @@ class Settings:
 
 		# Alien settings
 		self.alien_speed = 1.0
-		self.fleet_drop_speed = 10
+		self.fleet_drop_speed = 50
 		# fleet_direction of 1 represents right; -1 represents left.
 		self.fleet_direction = 1
 
