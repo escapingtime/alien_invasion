5e9b4cf77f77b39d07a66454aacd2ed4db9c31a8
escapingtime <gharris@mobileinstallusa.com>
2022-08-24 16:18:23 -0700

    Functionality for resetting screen via _ship_hit() added.

--- alien_invasion.py
+++ alien_invasion.py
@@ -114,7 +114,7 @@ class AlienInvasion:
 
 		# Look for alien-ship collisions.
 		if pygame.sprite.spritecollideany(self.ship, self.aliens):
-			print("Ship hit!!!")
+			self._ship_hit()
 
 	def _create_fleet(self):
 		"""Create the fleet of aliens."""
